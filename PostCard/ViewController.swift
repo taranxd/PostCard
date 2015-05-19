f09f2ecc4446e780487bc819da75d491688de7dd
taran <taran@tarans-MacBook-Air.local>
2015-05-19 22:30:51 +0530

    Name Label Update

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +27,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        nameLabel.hidden = false;
+        nameLabel.text = "-" + enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor();
+        enterNameTextField.text = "";
+        enterNameTextField.resignFirstResponder();
+        
+        
         messageLabel.hidden = false;
         messageLabel.text = enterMessageTextField.text;
         messageLabel.textColor = UIColor.redColor();
-        
         enterMessageTextField.text = "";
         enterMessageTextField.resignFirstResponder();
         
